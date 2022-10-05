f098d821f673a412fb36ec9857ac5fd53f349e6e
seanee3670 <cseok3670@gmail.com>
2022-10-05 10:36:49 +0900

    [update] 1005 Calculator - updated divide method to return double

--- src/java_1005/Calculator.java
+++ src/java_1005/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
         return a * b;
     }
 
-    public int divide(int a, int b) {
-        return a / b;
+    public double divide(int a, int b) {
+        return (double) a / (double) b;
     }
 }
