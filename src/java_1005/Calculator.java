8d3db79465154e8e0df7e0fc59f05fb70f880127
seanee3670 <cseok3670@gmail.com>
2022-10-05 10:49:04 +0900

    [update] 1005 Calculator - created properties and a constructor to reuse the variables

--- src/java_1005/Calculator.java
+++ src/java_1005/Calculator.java
@@ -2,23 +2,34 @@ package java_1005;
 
 public class Calculator {
 
-    /*
+    private int a;
+    private int b;
+
+    public Calculator() {}
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+/*
     더하기 빼기 곱하기 나누기
      */
 
-    public int add(int a, int b) {
+    public int add() {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public int subtract() {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public int multiply() {
         return a * b;
     }
 
-    public double divide(int a, int b) {
+    public double divide() {
         return (double) a / (double) b;
     }
 }
--- src/java_1005/CalculatorMain.java
+++ src/java_1005/CalculatorMain.java
@@ -3,12 +3,12 @@ package java_1005;
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
+        Calculator cal = new Calculator(10, 20);
 
-        System.out.println(cal.add(10, 20));
-        System.out.println(cal.subtract(10, 20));
-        System.out.println(cal.multiply(10, 20));
-        System.out.println(cal.divide(10, 20));
+        System.out.println(cal.add());
+        System.out.println(cal.subtract());
+        System.out.println(cal.multiply());
+        System.out.println(cal.divide());
 
     }
 }
