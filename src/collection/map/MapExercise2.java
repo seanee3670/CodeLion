e164fbced40a674d13337b975a4466aec33d7525
seanee3670 <cseok3670@gmail.com>
2022-10-06 11:46:15 +0900

    [init] 221006 Map Exercise2 - Count Number of alphabets in a String (didn't use Map though..)

---
+++ src/collection/map/MapExercise2.java
@@ -0,0 +1,26 @@
+package collection.map;
+
+public class MapExercise2 {
+
+    public static void main(String[] args) {
+
+        // 특정 String 에 있는 알파벳 개수 세기
+        // String s에 있는 a~z까지 알파벳의 개수를 세는 알고리즘을 구현 해보세요 (대소문자 불문)
+        // array 로 해결 가능
+
+        String s = "a2f39S8h^f2l";
+
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if ( (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') ) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
