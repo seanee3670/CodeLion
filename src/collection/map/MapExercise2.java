b7dd89aa6710cab2998148577e8fbf57a5c2b4d5
seanee3670 <cseok3670@gmail.com>
2022-10-06 13:20:58 +0900

    [update] 221006 Map Exercise2 - added 'isAlphabet' boolean method

--- src/collection/map/MapExercise2.java
+++ src/collection/map/MapExercise2.java
@@ -6,6 +6,7 @@ public class MapExercise2 {
         // 특정 String 에 있는 알파벳 개수 세기
         // String s에 있는 a~z까지 알파벳의 개수를 세는 알고리즘을 구현 해보세요 (대소문자 불문)
         // array 로 해결 가능
+
         String s = "a2f39S8h^f2l";
         int cnt = 0;
 
