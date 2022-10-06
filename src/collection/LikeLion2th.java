5ef2f5eea6e9c96bd9f34d0290c1ea31e388acbf
seanee3670 <cseok3670@gmail.com>
2022-10-06 09:25:17 +0900

    [update] 221006 code refactoring

---
+++ src/collection/LikeLion2th.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeLion2th {
+
+    private List<String> students;
+
+    public LikeLion2th() {
+        this.students = new ArrayList<>();
+        this.students.add("석시윤");
+    }
+
+    public List<String> getStudentList() {
+        return this.students;
+    }
+
+}
---
+++ src/collection/Student.java
@@ -0,0 +1,13 @@
+package collection;
+
+public class Student {
+    private int classNo;
+    private String name;
+    private String email;
+
+    public Student(int classNo, String name, String email) {
+        this.classNo = classNo;
+        this.name = name;
+        this.email = email;
+    }
+}
