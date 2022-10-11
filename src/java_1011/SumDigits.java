25e4dfbb23b95da5a6654be5fa9ac78c90252e06
seanee3670 <cseok3670@gmail.com>
2022-10-11 09:20:30 +0900

    [update] 221011 SumDigits - 자릿수 더하기

---
+++ src/java_1011/SumDigits.java
@@ -0,0 +1,14 @@
+package java_1011;
+
+public class SumDigits {
+    public int SumDigits(int n) {
+        int sum = 0;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
