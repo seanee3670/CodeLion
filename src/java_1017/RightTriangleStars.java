f3612b116d328d46c7ddf8e2c79e5e3907e25335
seanee3670 <cseok3670@gmail.com>
2022-10-17 09:24:27 +0900

    [init] 221017 - RightTriangleStarts

---
+++ src/java_1017/RightTriangleStars.java
@@ -0,0 +1,20 @@
+package java_1017;
+
+public class RightTriangleStars {
+    public void print() {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+
+    public void printLoop(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
---
+++ src/java_1017/RightTriangleStarsMain.java
@@ -0,0 +1,11 @@
+package java_1017;
+
+public class RightTriangleStarsMain {
+    public static void main(String[] args) {
+        RightTriangleStars rightTriangleStars = new RightTriangleStars();
+
+        rightTriangleStars.print();
+
+        rightTriangleStars.printLoop(5);
+    }
+}
