10e4bf68dd9825ac65458a915ec1aa6600d974f1
seanee3670 <cseok3670@gmail.com>
2022-10-20 10:31:50 +0900

    [init] 221020 - Stack

---
+++ src/java_1020/Test/java_1020/Main/StackTest.java
@@ -0,0 +1,35 @@
+package java_1020.Main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void push() {
+        
+    }
+
+    @Test
+    void getStack() {
+    }
+
+    @Test
+    void getArrById() {
+    }
+
+    @Test
+    void isEmpty() {
+        return  == 0;
+    }
+
+    @Test
+    void pop() {
+    }
+}
