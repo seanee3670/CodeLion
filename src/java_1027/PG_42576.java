886ce1d1bc1dba77c3c75c240a44c07bd44a534a
seanee3670 <cseok3670@gmail.com>
2022-10-27 09:46:06 +0900

    [update] 221027 Programmers 완주하지 못한 선수
    refactored

--- src/java_1027/PG_42576.java
+++ src/java_1027/PG_42576.java
@@ -7,13 +7,11 @@ public class PG_42576 {
         Arrays.sort(participant);
         Arrays.sort(completion);
 
-        int pLen = participant.length - 1;
+        String answer = participant[0];
 
-        String answer = participant[pLen];
-
-        for (int i = 0; i < pLen; i++) {
+        for (int i = 0; i < completion.length; i++) {
             if (!participant[i].equals(completion[i])) {
-                return participant[i];
+                answer = participant[i];
             }
 
         }
