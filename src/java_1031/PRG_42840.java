38df320ec53b839940fd6ada4c37c9904cf13cc4
seanee3670 <cseok3670@gmail.com>
2022-10-31 10:00:25 +0900

    [init] 221031 Programmers 모의고사
    solved w/ Arraylist

---
+++ src/java_1031/PRG_42840.java
@@ -0,0 +1,93 @@
+package java_1031;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PRG_42840 {
+    public int[] solution(int[] answers) {
+        /*
+        1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+        2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+        3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+         */
+
+        int[] scores = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            if (isFirstSupojaAnswer(i, answers[i])) {
+                scores[0]++;
+            }
+            if (isSecondSupojaAnswer(i, answers[i])) {
+                scores[1]++;
+            }
+            if (isThirdSupojaAnswer(i, answers[i])) {
+                scores[2]++;
+            }
+        }
+
+        int maxScore = Math.max(Math.max(scores[0], scores[1]), scores[2]);
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == maxScore) {
+                list.add(i + 1);
+            }
+        }
+
+//        return list.stream().mapToInt(i -> i).toArray(); // 테스트케이스 몇개 통과 하지 못함..
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+
+    static boolean isFirstSupojaAnswer(int idx, int answer) {
+        if (idx % 5 + 1 == answer) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean isSecondSupojaAnswer(int idx, int answer) {
+
+        int supojaAnswer = 0;
+        int remainder = idx % 8;
+
+        if (idx % 2 == 0) {
+            supojaAnswer = 2;
+        }
+        else if (remainder == 1) {
+            supojaAnswer = 1;
+        } else if (remainder == 3) {
+            supojaAnswer = 3;
+        } else if (remainder == 5) {
+            supojaAnswer = 4;
+        } else if (remainder == 7) {
+            supojaAnswer = 5;
+        }
+
+        return supojaAnswer == answer ? true : false;
+    }
+
+    static boolean isThirdSupojaAnswer(int idx, int answer) {
+        int supojaAnswer = 0;
+        int remainder = idx % 10;
+
+        if (remainder == 0 || remainder == 1) {
+            supojaAnswer = 3;
+        } else if (remainder == 2 || remainder == 3) {
+            supojaAnswer = 1;
+        } else if (remainder == 4 || remainder == 5) {
+            supojaAnswer = 2;
+        } else if (remainder == 6 || remainder == 7) {
+            supojaAnswer = 4;
+        } else if (remainder == 8 || remainder == 9) {
+            supojaAnswer = 5;
+        }
+        return supojaAnswer == answer ? true : false;
+    }
+}
