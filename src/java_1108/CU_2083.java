4c821e2e2c97c6a8c1f265784b405ed402054c1e
seanee3670 <cseok3670@gmail.com>
2022-11-08 10:35:09 +0900

    [init] 221108 CodeUp 2083 이분 탐색 (선형)

---
+++ src/java_1108/CU_2083.java
@@ -0,0 +1,38 @@
+package java_1108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CU_2083 { // 이분탐색
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int key = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int result = Arrays.binarySearch(arr, key);
+
+        if (result < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result + 1);
+        }
+
+
+
+
+    }
+}
